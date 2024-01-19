7496389338acb09b6915c9ff38677f590607e18d
asorin.alexia <asorin.alexia@gmail.com>
2024-01-19 14:15:38 +0100

    feat(spring): initialisation du projet SpringBoot

---
+++ back/fitmarket/src/main/java/com/m2/tiila/fitmarket/FitmarketApplication.java
@@ -0,0 +1,13 @@
+package com.m2.tiila.fitmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmarketApplication.class, args);
+	}
+
+}
