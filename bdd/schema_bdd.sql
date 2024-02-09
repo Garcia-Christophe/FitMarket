da7aba29940e00c23139cf9b22ae428ebd3097b0
G.Christophe <garciachristophe29@gmail.com>
2024-02-09 15:31:47 +0100

    feat(bdd): ajout des valeurs

--- bdd/schema_bdd.sql
+++ bdd/schema_bdd.sql
@@ -1,5 +1,8 @@
-DROP SCHEMA `fit_market` ;
-CREATE SCHEMA `fit_market` ;
+-- Suppression du schéma existant
+DROP SCHEMA IF EXISTS `fit_market`;
+CREATE SCHEMA `fit_market`;
+
+-- Création des tables
 CREATE TABLE fit_market.utilisateur (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(50) NOT NULL,
@@ -7,35 +10,48 @@ CREATE TABLE fit_market.utilisateur (
     email VARCHAR(100) UNIQUE NOT NULL,
     mot_de_passe VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE fit_market.produit (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
     prix DECIMAL(10, 2) NOT NULL,
-    image VARCHAR(255),
     description_produit TEXT
 );
+
+CREATE TABLE fit_market.image_produit (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_produit INT,
+    image VARCHAR(255),
+    FOREIGN KEY (id_produit) REFERENCES produit(id)
+);
+
 CREATE TABLE fit_market.cours (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(100) NOT NULL,
     video VARCHAR(255),
-    description_produit TEXT,
     jours_heures VARCHAR(255),
     duree INT,
     adresse VARCHAR(255),
     id_produit INT,
     FOREIGN KEY (id_produit) REFERENCES produit(id)
 );
+
 CREATE TABLE fit_market.commentaire (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titre VARCHAR(100) NOT NULL,
     contenu TEXT NOT NULL,
     date_heure DATETIME DEFAULT CURRENT_TIMESTAMP,
-    image VARCHAR(255),
     note INT CHECK (note >= 0 AND note <= 5),
     id_utilisateur INT,
     id_produit INT,
     FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id),
     FOREIGN KEY (id_produit) REFERENCES produit(id)
+);
+
+CREATE TABLE fit_market.image_commentaire (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_commentaire INT,
+    image VARCHAR(255),
+    FOREIGN KEY (id_commentaire) REFERENCES commentaire(id)
 );
 
 CREATE TABLE fit_market.commande (
